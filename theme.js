ab38872c2342c08530f02db5b8f7d88f843dbc61
sagarregmi2056 <sagarregmi2056@gmail.com>
2025-05-11 18:40:44 +0000

    fix:admin panel error

--- theme.js
+++ theme.js
@@ -1,10 +1,25 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
-}
-
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'white',
+      }
+    }
+  },
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  },
+})
 
 export default theme 
